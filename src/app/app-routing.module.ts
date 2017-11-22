d51f7a0c1ef51dc8b8e9328b49190ef507dd1a8d
Cristian Aleman <cristianalemanfuentes@gmail.com>
2017-11-22 16:16:59 -0600

    Comunicacion entre componentes Padre e Hijo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TiendaComponent } from '../app/tienda/tienda.component';
+import { PageNotFoundComponent } from '../app/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: 'home', component: TiendaComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TiendaComponent } from './tienda/tienda.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ParquesComponent } from './parques/parques.component';
+import { NgModel, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TiendaComponent,
+    PageNotFoundComponent,
+    ParquesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
